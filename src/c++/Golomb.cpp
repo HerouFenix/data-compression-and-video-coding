262df917882fb6d7838b1f66a55965c155f86b2c
HerouFenix <diogo04@ua.pt>
2019-11-21 17:46:04 +0000

    started golomb

---
+++ src/c++/Golomb.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+#include <exception>
+#include <fstream>
+#include <iterator>
+
+using namespace std;
+
+class Golomb
+{
+private:
+    int encoding_parameter;
+    int output_bits;
+
+public:
+    Golomb(int m)
+    {
+        encoding_parameter = m;
+        output_bits 
+    }
+
+    /* GETTERS & SETTERS */
+
+};
+
+
+int main()
+{
+    return 0;
+}
