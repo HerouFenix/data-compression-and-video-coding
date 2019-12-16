7e47454b7637115abe9659d7afacf85ab8072115
HerouFenix <diogo04@ua.pt>
2019-12-16 11:55:03 +0000

    GOMBYY

--- src/python/VideoCodec.py
+++ src/python/VideoCodec.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2 as cv2
 from Frame import *
 
+
 class VideoCodec:
 
     def __init__(self, file_path):
@@ -12,73 +13,76 @@ class VideoCodec:
             header = (frame_reader.readline()).decode("UTF-8")
         header_info = header.split(" ")
 
+        frame_type = "420"
+
         for info in header_info:
             if info[0] == "W":
                 self.width = int(info.replace("W", ""))
             elif info[0] == "H":
-                self.height = int(info.replace("H", ""))  
-            elif info[0] == "F":      
+                self.height = int(info.replace("H", ""))
+            elif info[0] == "F":
                 self.fps = info.replace("F", "")
-            elif info[0] == "C":  
-                info = info.replace("C","")
+            elif info[0] == "C":
+                info = info.replace("C", "")
                 if info.startswith("420"):
-                    self.frame_type = "420"
+                    frame_type = "420"
                 if info.startswith("422"):
-                    self.frame_type = "422"
+                    frame_type = "422"
                 if info.startswith("444"):
-                    self.frame_type = "444"
+                    frame_type = "444"
 
-        if self.frame_type == None:
-            self.frame_type = "420"
-
+        if frame_type == "420":
+            self.frame = Frame420(self.height, self.width)
+        if frame_type == "422":
+            frame_type = Frame422(self.height, self.width)
+        if frame_type == "444":
+            frame_type = Frame444(self.height, self.width)
 
     def play_video(self):
         with open(self.file_path, "rb") as stream:
 
             line = stream.readline()
-            #print(line)
-            frame = Frame420(self.height, self.width)
+            # print(line)
+
             while True:
                 line = stream.readline()
-                frame.set_frame(stream)
-                BGR = frame.show_frame()
+                self.frame.set_frame(stream)
+                BGR = self.frame.show_frame()
 
                 # Display the image with OpenCV
                 cv2.imshow('image', BGR)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
             cv2.destroyAllWindows()
-    
+
     def decompress_frame(self, mode="JPEG-1"):
-        if mode not in ["JPEG-"+str(i) for i in range(1,8)] and mode != "JPEG-LS":
+        if mode not in ["JPEG-"+str(i) for i in range(1, 8)] and mode != "JPEG-LS":
             print("Invalid mode")
             return None
         with open(self.file_path, "rb") as stream:
             line = stream.readline()
-            
-            frame = Frame420(self.height, self.width)
+
             while True:
                 line = stream.readline()
-                frame.set_frame(stream)
+                self.frame.set_frame(stream)
 
     def compress_video(self, compress_path, mode="JPEG-1"):
-        if mode not in ["JPEG-"+str(i) for i in range(1,8)] and mode != "JPEG-LS":
+        if mode not in ["JPEG-"+str(i) for i in range(1, 8)] and mode != "JPEG-LS":
             print("Invalid mode")
             return None
         with open(self.file_path, "rb") as stream:
             line = stream.readline()
-            
-            frame = Frame420(self.height, self.width)
+
             while True:
                 line = stream.readline()
-                frame.set_frame(stream)
+                self.frame.set_frame(stream)
 
-                compressed_frame = frame.compress_frame(mode)
-                
+                compressed_frame = self.frame.compress_frame(mode)
+
                 for f in compressed_frame:
-                    for x in np.nditer(a):
+                    for x in np.nditer(f):
                         print(x, end=' ')
-                
+
 
 if __name__ == "__main__":
     codec = VideoCodec("../../tests/vids/ducks_take_off_1080p50.y4m")
